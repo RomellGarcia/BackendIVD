140a3618f2809fc57dbf1136aaec691911fea057
RomellGarcia <aatroxlunag@gmail.com>
2026-06-04 10:40:49 -0600

    RECREACION EN 0

--- src/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-//src/routes/login.routes.js
-var express    = require('express');
-var router     = express.Router();
-var controller = require('../controllers/login.controller');
-
-router.post('/', controller.iniciarSesion);
-
-module.exports = router;
